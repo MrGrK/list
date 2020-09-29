e5e8b14452c35e8240157eb9b324d4ba805a6363
MrGrK <grigksh@gmail.com>
2020-09-30 00:12:08 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] sArr = {"кошка", "resa", "asdq", "asdq", "grt","a" ,"a",
+                         "b","c" ,"aqbb", "Dog", "jkbvjj", "tgj", "c"};
+    }
+}
